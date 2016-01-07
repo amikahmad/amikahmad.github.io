d6c773035fa2c1c22e2e0f37c8192cd426c58589
amikahmad <amikahmad@gmail.com>
2016-01-07 15:35:36 -0500

    added color morph javascript

--- js/colorMorph.js
+++ js/colorMorph.js
@@ -1,16 +1,16 @@
 $(function(){
-  $("#morph").addClass( "color0" );
+  $("article").addClass( "color0" );
 });
 var myFunction = function(){
-  $("#morph").switchClass( "color0", "color1", 2000 );
-  $("#morph").switchClass( "color1", "color2", 2000 );
-  $("#morph").switchClass( "color2", "color3", 2000 );
-  $("#morph").switchClass( "color3", "color4", 2000 );
-  $("#morph").switchClass( "color4", "color5", 2000 );
-  $("#morph").switchClass( "color5", "color6", 2000 );
-  $("#morph").switchClass( "color6", "color7", 2000 );
-  $("#morph").switchClass( "color7", "color8", 2000 );
-  $("#morph").switchClass( "color8", "color9", 2000 );
-  $("#morph").switchClass( "color9", "color0", 2000 );
+  $("article").switchClass( "color0", "color1", 2000 );
+  $("article").switchClass( "color1", "color2", 2000 );
+  $("article").switchClass( "color2", "color3", 2000 );
+  $("article").switchClass( "color3", "color4", 2000 );
+  $("article").switchClass( "color4", "color5", 2000 );
+  $("article").switchClass( "color5", "color6", 2000 );
+  $("article").switchClass( "color6", "color7", 2000 );
+  $("article").switchClass( "color7", "color8", 2000 );
+  $("article").switchClass( "color8", "color9", 2000 );
+  $("article").switchClass( "color9", "color0", 2000 );
 };
 setInterval(myFunction, 0, 20000);
