21643a7bca6aae042c6069167b68406f4fbc16e4
amikahmad <amikahmad@gmail.com>
2016-01-07 22:22:18 -0500

    final pages layout part 1

--- js/colorMorph.js
+++ js/colorMorph.js
@@ -1,16 +1,16 @@
-$(function(){
-  $("article").addClass( "color0" );
+$(function() {
+  $("#box").addClass("color0");
 });
-var myFunction = function(){
-  $("article").switchClass( "color0", "color1", 2000 );
-  $("article").switchClass( "color1", "color2", 2000 );
-  $("article").switchClass( "color2", "color3", 2000 );
-  $("article").switchClass( "color3", "color4", 2000 );
-  $("article").switchClass( "color4", "color5", 2000 );
-  $("article").switchClass( "color5", "color6", 2000 );
-  $("article").switchClass( "color6", "color7", 2000 );
-  $("article").switchClass( "color7", "color8", 2000 );
-  $("article").switchClass( "color8", "color9", 2000 );
-  $("article").switchClass( "color9", "color0", 2000 );
+var myFunction = function() {
+  $("#box").switchClass("color0", "color1", 2000);
+  $("#box").switchClass("color1", "color2", 2000);
+  $("#box").switchClass("color2", "color3", 2000);
+  $("#box").switchClass("color3", "color4", 2000);
+  $("#box").switchClass("color4", "color5", 2000);
+  $("#box").switchClass("color5", "color6", 2000);
+  $("#box").switchClass("color6", "color7", 2000);
+  $("#box").switchClass("color7", "color8", 2000);
+  $("#box").switchClass("color8", "color9", 2000);
+  $("#box").switchClass("color9", "color0", 2000);
 };
 setInterval(myFunction, 0, 20000);
