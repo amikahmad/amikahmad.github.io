52fd17134594bf283b8b7bf966f8a7a751ecedf3
amikahmad <amikahmad@gmail.com>
2016-01-11 23:36:49 -0500

    added javascript

--- js/hover.js
+++ js/hover.js
@@ -1,3 +1,11 @@
-$(".letter").hover(function() {
-        $(this).closest("#word").toggleClass("hovered")
-    });
+$(".experience").hover(function () {
+    $("#color").addClass("experience-border");
+}, function () {
+    $("#color").removeClass("experience-border");
+});
+
+$(".awards").hover(function () {
+    $("#color").addClass("awards-border");
+}, function () {
+    $("#color").removeClass("awards-border");
+});
