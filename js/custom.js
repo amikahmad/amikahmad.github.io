cbccf6de2182bc536c2ed16e376b4dc2c79b6305
amikahmad <amikahmad@gmail.com>
2016-01-16 00:28:58 -0500

    script changes

--- js/custom.js
+++ js/custom.js
@@ -36,3 +36,30 @@ $(".slide").hover(function () {
     $("#color").removeClass("slide-border");
 });
 
+var mobile;
+    if ((/iPhone|iPod|Android|BlackBerry/).test(navigator.userAgent)) {
+        mobile = true;
+    } else {
+        mobile = false;
+    }
+
+    adjustHeight();
+
+    $(window).resize(function() {
+        adjustHeight();
+    });
+
+    function adjustHeight() {
+        if (!mobile) {
+            var height = $(window).height();
+            var adjust = height;
+
+            if (height > 400) {
+                $("#description ").css({
+                    "margin-top ": adjust / 2 - 120 + "px ",
+                    "padding-bottom ": adjust / 2 - 150 + "px "
+                });
+            }
+        }
+
+
