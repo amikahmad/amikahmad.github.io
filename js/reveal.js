2c0ed831bf27e8da89de4826f53258542b602a29
amikahmad <amikahmad@gmail.com>
2016-01-16 00:26:53 -0500

    script changes

--- js/reveal.js
+++ js/reveal.js
@@ -1,16 +1,38 @@
-
 var active = false;
-$('.slide').click(function() {
+    $('.slide').click(function() {
         $(".slide").toggle();
-    if (!active) {
-        $('#main').fadeOut(200);
-        $('#plus').delay(200).fadeIn(200);
-        $('.slide').css({'color': '#707070'});
-        active = true;
-    } else {
-        $('#plus').fadeOut(200);
-        $('#main').delay(200).fadeIn(200);
-        $('.slide').css({'color': '#505050'});
-        active = false;
-    }
+        if (!active) {
+            $('#main').fadeOut(300);
+            $('#plus').delay(300).fadeIn(300);
+            $('.slide').css({
+                'color': '#707070'
+            });
+            active = true;
+        } else {
+            $('#plus').fadeOut(300);
+            $('#main').delay(300).fadeIn(300);
+            $('.slide').css({
+                'color': '#505050'
+            });
+            active = false;
+        }
+    });
+
+    $(".experience").hover(function () {
+    $("#color").addClass("experience-border");
+}, function () {
+    $("#color").removeClass("experience-border");
 });
+
+$(".awards").hover(function () {
+    $("#color").addClass("awards-border");
+}, function () {
+    $("#color").removeClass("awards-border");
+});
+
+$(".slide").hover(function () {
+    $("#color").addClass("slide-border");
+}, function () {
+    $("#color").removeClass("slide-border");
+});
+
